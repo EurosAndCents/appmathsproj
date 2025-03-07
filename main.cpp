9c98b2f1b6ebf6596bf045ab4f4ecfd528ff3226
eurosandcents <2kul4yhu@gmail.com>
2025-03-07 15:28:13 +0000

    v1.1.0 - Added constants K_fidelity, K_init_population, K_birth_rate, K_death_rate, K_effective_rate (Required assumptions are written in comments) - Derived population formula for current population  (Y axis): y(t)=Ce^[rt]

--- main.cpp
+++ main.cpp
@@ -5,17 +5,28 @@
 int main() {
     using namespace matplot;
 
+    // Variables
+    constexpr double K_init_population{1000}; // Assume a deer population of 1000 (red deer, fallow deer, sika deer) -> each give birth to ~1 fawn per year
+    constexpr double K_fidelity{0.01f}; // Fidelity of the graph
+
+    constexpr double K_birth_rate{0.5*0.5}; // Assume 50% are female, and 50% giving birth each year
+    constexpr double K_death_rate{0.2}; // Assume 20% die per year
+    constexpr double K_effective_rate{K_birth_rate - K_death_rate}; // Effective rate of population growth
+
     // Generate data
     std::vector<double> x = []() {
         std::vector<double> gen{};
-        for (int i=0; i<1*1000; i++)
-            gen.push_back(i*0.01); // X should not be touched - determines the "fidelity" of the graph
+        for (int i=0; i<100*static_cast<int>(1/K_fidelity); i++) // i=1000 = 1 year, going to 100 years
+            gen.push_back(i*K_fidelity); // Do not touch - determines the "fidelity" of the graph
         return gen;
     }();
     std::vector<double> y = [&]() {
         std::vector<double> gen{};
-        for (auto i : x) {
-            gen.push_back(std::sin(i)); // Straight line: y=x
+        for (int i=0; i<x.size(); i++) {
+            const double time = x[i];
+            double population = K_init_population * pow(M_E, (K_effective_rate*time)); // P=Ce^(rt)
+
+            gen.push_back(population); // y=Pe^(rt)
         }
         return gen;
     }();
