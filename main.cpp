012e624911b2c67dce9163214fef0e24658239c2
eurosandcents <2kul4yhu@gmail.com>
2025-03-07 16:41:25 +0000

    v1.1.1 - Changes rates from being constant as they are prepared to be updated for seasons e.g hunting season - Solved warning datablock issue by ignoring warnings with new .gnuplot file - MatPlot++ bug - Using current_axes pointer and figure pointer to access the multiplot for efficiency

--- main.cpp
+++ main.cpp
@@ -3,28 +3,33 @@
 #include <vector>
 
 int main() {
-    using namespace matplot;
+    using namespace std;
 
     // Variables
-    constexpr double K_init_population{135000}; // Estimations online say 120k-150k, so assume 135k
-    constexpr double K_fidelity{0.01f}; // Fidelity of the graph
+    constexpr double K_init_population{1000}; // Assume a deer population of 1000 (red deer, fallow deer, sika deer) -> each give birth to ~1 fawn per year
+            constexpr double K_fidelity{0.01f}; // Fidelity of the graph
 
-    constexpr double K_birth_rate{0.5*0.5}; // Assume 50% are female, and 50% giving birth each year
-    constexpr double K_death_rate{0.2}; // Assume 20% die per year
-    constexpr double K_effective_rate{K_birth_rate - K_death_rate}; // Effective rate of population growth
+    // These rates are updated below in lambda func y=[](){}, they are here for reference
+    double birth_rate{0.5f*0.5f}; // Assume 50% are female, and 50% giving birth each year
+    double death_rate{0.2f}; // Assume 20% die per year - fluctuates due to hunting season (September-December @ 9/12-12/12)
+    double effective_rate{birth_rate - death_rate}; // Effective rate of population growth
 
     // Generate data
-    std::vector<double> x = []() {
-        std::vector<double> gen{};
-        for (int i=0; i<100*static_cast<int>(1/K_fidelity); i++) // i=1000 = 1 year, going to 100 years
+    vector<double> x = []() {
+        vector<double> gen{};
+        for (int i=0; i<100*static_cast<int>(1.0f/K_fidelity); i++) { // i=1000 = 1 year, going to 100 years
             gen.push_back(i*K_fidelity); // Do not touch - determines the "fidelity" of the graph
+        }
         return gen;
     }();
-    std::vector<double> y = [&]() {
-        std::vector<double> gen{};
-        for (int i=0; i<x.size(); i++) {
-            const double time = x[i];
-            double population = K_init_population * pow(M_E, (K_effective_rate*time)); // P=Ce^(rt)
+
+    vector<double> y = [&]() {
+        vector<double> gen{};
+        for (double time : x) {
+            if (fmod(time, 1.0f) > (9/12))
+                cout << "Hunting season @ " << time << " years" << endl;
+            effective_rate = birth_rate - death_rate;
+            double population = K_init_population * pow(M_E, (effective_rate*time)); // P=Ce^(rt)
 
             gen.push_back(population); // y=Pe^(rt)
         }
@@ -32,12 +37,16 @@ int main() {
     }();
 
 
+    using namespace matplot;
 
-    // Create plot
-    plot(x, y);
-    title("Deer Population Model");
-    xlabel("Years");
-    ylabel("Population");
+    // Create plot + datablock
+    auto fig = figure();
+    auto axes = fig->current_axes();
+    axes->hold(true);
+    axes->plot(x, y);
+    axes->title("Deer Population Model");
+    axes->xlabel("Years");
+    axes->ylabel("Population");
 
     // Display plot
     show();
